d96afdafb44aa6fde3069bd9b3e672f4d54b6d0a
vinay <vgot0001@student.monash.edu>
2025-03-14 10:08:31 +1100

    added python code to make api calls to frontend.

--- onboarding-frontend/src/main.js
+++ onboarding-frontend/src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
 import { createPinia } from "pinia";
-
 const pinia = createPinia();
+
 
 createApp(App).use(Antd).use(router).use(pinia).mount("#app");
